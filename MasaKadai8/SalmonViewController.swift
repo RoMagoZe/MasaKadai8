a536b17011ef3314f9f95ce859b237f70e1a2148
masaharu <dabao456democebu3@gmail.com>
2023-01-10 11:15:19 +0900

    課題8

---
+++ MasaKadai8/SalmonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SalmonViewController.swift
+//  MasaKadai8
+//
+//  Created by Mina on 2023/01/09.
+//
+
+import UIKit
+
+class SalmonViewController: UIViewController {
+
+    @IBOutlet private weak var numberOfLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+
+    @IBAction private func changeSlider(_ sender: Any) {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.sliderDelgate = "\(slider.value)"
+        numberOfLabel.text = "\(slider.value)"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        numberOfLabel.text = delegate?.sliderDelgate
+        delegate?.sliderPoint = slider
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.sliderPoint.value = slider.value
+    }
+}
